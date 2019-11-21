c6ea09124da3e09ceca4a4762c12f73ca4d34211
Elom Amouzou <elom1990@yahoo.com>
2019-11-21 13:37:07 -0700

    Step 4

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,25 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+def standard_deviation
+  ages = [24, 30, 18, 20, 41]
+  ages_sum = ages.sum #133
+  ages_count = ages.count #5
+  ages_mean = ages_sum / ages_count
+  ages_minus_mean = []
+  ages.each do |age|
+    ages_minus_mean << age - ages_mean
+  end
+  ages_squared = []
+  ages_minus_mean.each do |age|
+    ages_squared << age ** 2
+  end
+  p ages_squared
+
+
+
+
+
+end
 
 # When you find the standard deviation, print it out
+p standard_deviation
